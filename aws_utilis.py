d377bd41536ae44ff79feca417bac436e92038f6
Filippo Corsini <filippocorsini@MacBook-Pro-di-Filippo.local>
2024-01-22 21:10:11 +0100

    corretto errore su nome libreria

--- aws_utilis.py
+++ aws_utilis.py
@@ -1,9 +1,11 @@
 import boto3
 from botocore.exceptions import ClientError
+import botocore.config
 
 def get_aws_parameter(param_name, decrypt=False):
-    """Ottieni un parametro dal AWS Parameter Store."""
-    ssm = boto3.client('ssm')
+    """Ottieni un parametro da AWS Systems Manager Parameter Store."""
+    config = botocore.config.Config(region_name='eu-north-1')  # Usa solo il codice della regione
+    ssm = boto3.client('ssm', config=config)
     try:
         parameter = ssm.get_parameter(Name=param_name, WithDecryption=decrypt)
         return parameter['Parameter']['Value']
