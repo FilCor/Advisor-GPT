84c53c9b9858606c1efe6e1fe1a4ed84d5b4cf35
Filippo Corsini <filippocorsini@MacBook-Pro-di-Filippo.local>
2024-01-27 21:34:23 +0100

    aggiunto tool stock e wolfram alfa 12

--- tools/calculator_tools.py
+++ tools/calculator_tools.py
@@ -1,56 +1,27 @@
 from typing import Optional, Type
 from pydantic import BaseModel, Field
 from langchain.tools import BaseTool
-from langchain.callbacks.manager import CallbackManagerForToolRun  # Importazione aggiunta
-import wolframalpha
-import os
+from langchain.callbacks.manager import CallbackManagerForToolRun
+from langchain_community.utilities.wolfram_alpha import WolframAlphaAPIWrapper
 from aws_utilis import get_aws_parameter
-
-
-class WolframAlphaAPIWrapper(BaseModel):
-    """
-    Wrapper for Wolfram Alpha API.
-    This class simplifies the process of making queries to the Wolfram Alpha API.
-    """
-
-    def __init__(self, app_id: str):
-        super().__init__()
-        self.wolfram_client = wolframalpha.Client(app_id)
-
-    def run(self, query: str) -> str:
-        """
-        Run a query through WolframAlpha and return the result.
-        
-        Args:
-            query (str): The query string to be sent to Wolfram Alpha.
-
-        Returns:
-            str: The result of the query.
-        """
-        try:
-            res = self.wolfram_client.query(query)
-            # Assuming the first 'pod' is the most relevant one
-            return next(res.results).text if res.results else "No results found."
-        except Exception as e:
-            return f"An error occurred: {str(e)}"
 
 # Recupero dell'APP ID da AWS
 app_id = get_aws_parameter("WOLFRAM_ALPHA_APPID", decrypt=True)
 
-# Assicurati che l'input per lo strumento sia definito
 class WolframAlphaInput(BaseModel):
+    """Input model for Wolfram Alpha tool."""
     query: str = Field(description="Query to be asked to Wolfram Alpha")
 
 class WolframAlphaTool(BaseTool):
+    """LangChain tool for querying Wolfram Alpha."""
     name = "Wolfram Alpha"
-    description = "Executes queries using Wolfram Alpha API"
+    description = "Usefull when you need to do math calculations!"
     args_schema: Type[BaseModel] = WolframAlphaInput
 
-    def __init__(self, app_id: str):
-        super().__init__() 
-        self.wolfram = WolframAlphaAPIWrapper(app_id)
+    def __init__(self):
+        super().__init__()
+        self.wolfram = WolframAlphaAPIWrapper(wolfram_alpha_appid=app_id)
 
     def _run(self, query: str, run_manager: Optional[CallbackManagerForToolRun] = None) -> str:
+        """Run the given query using the Wolfram Alpha API."""
         return self.wolfram.run(query)
-
-
